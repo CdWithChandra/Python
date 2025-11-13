ab6505aa1f4d840803daae93a14e496559b3746e
devops <csrcg19@gmail.com>
2025-11-13 08:57:12 +0530

    list methods

---
+++ List Type/ListMethods.py
@@ -0,0 +1,17 @@
+# Python List Methods
+# append(), extend(), insert()
+# remove(), pop(), sort()
+os=["linux","windows"]
+os.append("ubuntu")
+print(os)
+os.extend(["fedora","Suse"])
+print(os)
+os.insert(1,"Ubuntu")
+print(os)
+os.remove("windows")
+print(os)
+os.pop()
+print(os)
+numbers=[3,1,4,1,5]
+numbers.sort()
+print(numbers)
